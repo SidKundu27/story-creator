43bfab98027dc0fc930d856a494b75c253cb475c
Sid Kundu <sidkundu13@gmail.com>
2026-05-05 02:59:42 -0400

    feat: add VITE_API_BASE_URL to environment and configure axios base URL

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import './theme.css';
 import './index.css';
 import App from './App';
 import theme from './theme';
+
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+if (apiBaseUrl) {
+  axios.defaults.baseURL = apiBaseUrl;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
